dc7ac240ebe9dc26e26132c5cd69e5e7423c6ce2
Kalicharan Mohanta <kalicharan.mohanta1@gmail.com>
2020-07-13 21:51:20 +0530

    Completed Project
    Password Hacker: Each Method have different functionality and Accuracy

--- pass_hacker.py
+++ pass_hacker.py
@@ -1,6 +1,9 @@
-# write your code here
+'''Password Hacker: Each Method have different functionality'''
 import socket
 import itertools
+import json
+from datetime import datetime, timedelta
+from string import ascii_uppercase, ascii_lowercase, digits
 from sys import argv
 
 
@@ -9,7 +12,7 @@ class Connector:
         self.ip = ip_address
         self.port = int(port)
 
-    '''def connect(self):
+    def connect(self):
         alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2',
                     '3', '4', '5', '6', '7', '8', '9', '0']
@@ -30,11 +33,11 @@ class Connector:
         for i in range(1, len(alph)):
             for j in itertools.product(alph, repeat=i):
                 value = [''.join(k) for k in j]
-                yield ''.join(value)'''
+                yield ''.join(value)
 
     @staticmethod
-    def file_check():
-        with open('C:\\Users\\Kalicharan Mohanta\\Downloads\\passwords.txt', 'r+', encoding='utf-8') as file1:
+    def file_check(file_name):
+        with open(file_name, 'r+', encoding='utf-8') as file1:
             for file in file1:
                 container = file.replace('\n', '')
                 inp = container
@@ -59,20 +62,78 @@ class Connector:
                         temp += l
                     yield temp
 
-    def file_connect(self):
+    def file_connect(self, file_name):
         with socket.socket() as new_connection:
             new_connection.connect((self.ip, self.port))
-            file_value = self.file_check()
+            file_value = self.file_check(file_name)
             while True:
-                store1 = next(file_value)
-                new_connection.send(store1.encode())
+                store = next(file_value)
+                new_connection.send(store.encode())
                 response = new_connection.recv(5120)
                 response = response.decode()
-                print(response)
                 if response == 'Connection success!':
-                    print(store1)
+                    print(store)
                     break
+
+    # @staticmethod
+    # def permute():
+    #     with open('C:\\Users\\Kalicharan Mohanta\\Downloads\\passwords.txt', 'r+', encoding='utf-8') as file1:
+    #         for file in file1:
+    #             inp = file.replace('\n', '')
+    @staticmethod
+    def admin_pass(self, file_name, store1, new_connection):
+        file_value = self.file_check(file_name)
+        admin_data = dict(login=store1, password=' ')
+        while True:
+            store = next(file_value)
+            admin_data['password'] = store
+            new_connection.send(json.dumps(admin_data).encode())
+            response = new_connection.recv(5120).decode()
+            response = json.loads(response)
+            if response['result'] == 'Connection success!':
+                print(json.dumps(admin_data))
+                print(response)
+                break
+
+    def admin_login(self, file_name='', login=' ', password=' '):
+        with socket.socket() as new_connection:
+            new_connection.connect((self.ip, self.port))
+            file_value = self.file_check(file_name)
+            admin_data = dict(login=login, password=password)
+            flag = 1
+            while flag:
+                store = next(file_value)
+                admin_data['login'] = store
+                new_connection.send(json.dumps(admin_data).encode())
+                response = new_connection.recv(5120).decode()
+                response = json.loads(response)
+                if response['result'] == 'Wrong password!':
+                    password_string = list()
+                    count1 = 0
+                    while flag:
+                        for i in ascii_uppercase + ascii_lowercase + digits:
+                            if len(password_string) <= count1:
+                                password_string.append(str(i))
+                            else:
+                                password_string[count1] = i
+                            admin_data['password'] = ''.join(password_string)
+                            json_object = json.dumps(admin_data)
+                            try:
+                                new_connection.send(json_object.encode())
+                                start = datetime.now()
+                                response = new_connection.recv(1024).decode()
+                                finish = datetime.now()
+                                difference = finish - start
+                                response = json.loads(response)
+                                if difference >= timedelta(seconds=0.1):
+                                    count1 += 1
+                                elif response['result'] == 'Connection success!':
+                                    json_object = json.dumps(admin_data)
+                                    flag = 0
+                                    print(json_object)
+                            except ConnectionAbortedError:
+                                pass
 
 
 socket_connect = Connector(argv[1], argv[2])
-socket_connect.file_connect()
+socket_connect.admin_login(file_name='C:\\Users\\Kalicharan Mohanta\\Downloads\\logins.txt')
